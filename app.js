9869951e6d93b8e206446ba65bc865a514454a9c
Tim Heß <tim.hess@ima-ifu.rwth-aachen.de>
2019-05-31 21:29:37 +0200

    Add SessionHandler and routes

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+// Session Coockies TODO: Security
+var session = require('express-session');
+var MemoryStore = require('memorystore')(session);
+var sessionHandler = session({
+    secret: 'keyboard cat',
+    store: new MemoryStore({
+        checkPeriod: 86400000 // prune expired entries every 24h
+    }),
+    cookie: {}
+});
+
+var queue = require('./routes/Queue');
+app.use('/api/queue', sessionHandler, queue);
+
+var auth = require('./routes/Auth');
+app.use('/auth/', sessionHandler, auth);
+
+app.use(function(error, req, res, next) {
+    res.json({ message: error.message });
 });
 
 app.listen(3000, function () {
