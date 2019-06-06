0dfc2a2e228bc78ecb488842ea4c62856d7f0be4
Jonas Levin Weber <drjole@users.noreply.github.com>
2019-06-06 15:13:19 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='jamify',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='exp625, DrJoLe, Pallemann',
+    author_email='',
+    description='',
+    install_requires=['flask']
+)
